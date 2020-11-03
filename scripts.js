1e1d77b8af008497492cdfc140c06939ce9f8a0b
Asmaa Shaaban Ali <asm_sh_a@hotmail.com>
2020-11-03 20:36:07 +0200

    post requests working, yet no data is visually visible

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@
 const app = new Vue({
     el: '#tabs',
     data: {
-        searchTerm: '',
         aggregate: false,
         size: 0,
         offset: 0,
@@ -12,22 +11,49 @@ const app = new Vue({
     },
     methods: {
         searchTorrePeople() {
-            if (searchTerm != '') {
-                this.responseAvailable = false;
-                fetch("")
-                    .then(response => {
-                        if (response.ok) {
-                            return response.json()
-                        }
-                    })
-                    .then(response => {
-                        this.results = response.body;
-                        this.responseAvailable = true;
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    });
-            }
+            let _this = this;
+            const data = {
+                offset: _this.offset,
+                size: _this.size,
+                aggregate: this.aggregate
+            };
+            this.responseAvailable = false;
+            //url = 'https://search.torre.co/people/_search/?offset=' + this.offset + '&size=' + this.size + '&aggregate=' + this.aggregate;
+            axios.post('https://search.torre.co/people/_search/?offset=' + _this.offset + '&size=' + _this.size + '&aggregate=' + _this.aggregate)
+                .then(response => {
+                    return response;
+                })
+                .then(response => {
+                    debugger;
+                    this.results = response.data.results;
+                    this.responseAvailable = true;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+
+        },
+        searchTorreOpportunities() {
+            let _this = this;
+            const data = {
+                offset: _this.offset,
+                size: _this.size,
+                aggregate: this.aggregate
+            };
+            this.responseAvailable = false;
+            //url = 'https://search.torre.co/people/_search/?offset=' + this.offset + '&size=' + this.size + '&aggregate=' + this.aggregate;
+            axios.post('https://search.torre.co/opportunities/_search/?offset=' + _this.offset + '&size=' + _this.size + '&aggregate=' + _this.aggregate)
+                .then(response => {
+                    return response;
+                })
+                .then(response => {
+                    debugger;
+                    this.results = response.data.results;
+                    this.responseAvailable = true;
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
 })
