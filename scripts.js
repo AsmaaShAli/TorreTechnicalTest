72a4c4da4cdaf147f42bae4c41286930599192bf
Asmaa Shaaban Ali <asm_sh_a@hotmail.com>
2020-11-03 12:37:26 +0200

    first commit, empty vue page with basic functionality and tabbed components

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+const app = new Vue({
+    el: '#tabs',
+    data: {
+        activetab: 1,
+        results: [],
+        responseAvailable: false
+    },
+    methods: {
+        fetchAPIData() {
+        }
+    }
+})
