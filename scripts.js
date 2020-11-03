257827b2466bcfda33383517a74b415cd267a02b
Asmaa Shaaban Ali <asm_sh_a@hotmail.com>
2020-11-03 14:56:34 +0200

    second commit, updates on the front end and removing search text for name or role

--- scripts.js
+++ scripts.js
@@ -2,12 +2,32 @@
 const app = new Vue({
     el: '#tabs',
     data: {
+        searchTerm: '',
+        aggregate: false,
+        size: 0,
+        offset: 0,
         activetab: 1,
         results: [],
         responseAvailable: false
     },
     methods: {
-        fetchAPIData() {
+        searchTorrePeople() {
+            if (searchTerm != '') {
+                this.responseAvailable = false;
+                fetch("")
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json()
+                        }
+                    })
+                    .then(response => {
+                        this.results = response.body;
+                        this.responseAvailable = true;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }
         }
     }
 })
